6c071dcb66df842ec49ff144129fecc4082d4563
iGor McLven (Mauricio) <igormclven@gmail.com>
2021-12-15 15:23:44 -0500

    first deploy

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['fotografias.lasexta.com',
+              'miracomosehace.com'],
+  },
 }
---
+++ pages/todo.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import Image from 'next/image';
+
+function ToDoPage() {
+  const [ toDos, setToDos ] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(() => {
+      setIsLoading(true)
+      fetch('https://backend-pruebas-next.herokuapp.com/data')
+          .then(response => response.json())
+          .then(data => {
+              setToDos(data)
+              setIsLoading(false)
+          })
+          .catch(console.error);
+
+  }, [])
+  if (isLoading) {
+      return <p>Loading....</p>
+  }
+  if (!toDos) {
+      return <p>No List to show</p>
+  }
+  return (
+      <div>
+          <h1>TODO List</h1>
+          <ul>
+              {toDos.map( toDo =>
+                  <div class='card'>
+                      {toDo.fields.nombre} - <Image src={toDo.fields.url} width={200} height={200} />
+                  </div>
+              )}
+          </ul>
+      </div>
+  )
+}
+export default ToDoPage;
